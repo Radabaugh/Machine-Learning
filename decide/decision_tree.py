ef858fde43a8928b51cc7d0729e1369389c50093
radabaugh <radabaughtyler24@gmail.com>
2019-04-15 18:08:25 -0700

    decide p24, find the entropy

--- decide/decision_tree.py
+++ decide/decision_tree.py
@@ -1,4 +1,14 @@
+import math
 import pickle
+import collections
+
+def entropy(data):
+  frequency = collections.Counter([item[-1] for item in data])
+  def item_entropy(category):
+    ratio = float(category) / len(data)
+    return -1 * ratio * math.log(ratio, 2)
+  return sum(item_entropy(c) for c in frequency.values())
+
 
 with open("data", "rb") as f:
   L = pickle.load(f)
